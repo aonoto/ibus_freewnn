1102648a027d1dbe45dbeb9de204daf5fdacb707
Tomoki AONO <aonoto@users.sourceforge.jp>
2014-12-03 20:46:38 +0900

    Modify guard #define to __FWNN_H__.

--- src/fwnnserver.h
+++ src/fwnnserver.h
@@ -1,10 +1,10 @@
 /* vim:set et sts=4: */
 #ifndef __FWNN_H__
-#define __FWNN_H
+#define __FWNN_H__
 
 int fwnnserver_open();
 int fwnnserver_close();
 int fwnnserver_adddic(char *dicfilename);
 char *fwnnserver_kanren(char *yomi);
 
-#endif
+#endif	/* __FWNN_H__ */
